684e9ce6b9ce858ff879b2f45d7b14659bf5d347
a6002148 <srilakshmi.kommineni@bestbuy.com>
2019-09-19 00:33:10 -0500

    Search Query update

--- src/main/java/com/ivo/app/services/controller/FavoriteLocationController.java
+++ src/main/java/com/ivo/app/services/controller/FavoriteLocationController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
+import com.ivo.app.services.domain.LocationSearchRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -25,7 +26,7 @@ import com.ivo.app.services.service.LocationService;
 
 @RestController
 @RequestMapping(value="/location")
-public class LocationControler {
+public class FavoriteLocationController {
 	
 	@Autowired
 	private LocationService locationService;
@@ -33,32 +34,32 @@ public class LocationControler {
 	@Autowired
 	private LocationSearchService locationSearchService;
 	
-	@PostMapping(value="/v1/favorite/{uuid}/create/{id}")
-	public ResponseEntity<UserFavoriteLocationsXref> createUserFavoriteLocation(@PathVariable(value="uuid") String userUUID, @PathVariable(value="id") String restaurantUUID) throws URISyntaxException{
+	@PostMapping(value="/v1/favorite/{userUUID}/create/{locationUUID}")
+	public ResponseEntity<UserFavoriteLocationsXref> createUserFavoriteLocation(@PathVariable(value="userUUID") String userUUID, @PathVariable(value="locationUUID") String locationUUID) throws URISyntaxException{
 		
-		System.out.println("userUUID:"+userUUID+",restaurantUUID:"+restaurantUUID);
+		System.out.println("userUUID:"+userUUID+",locationUUID:"+locationUUID);
 		
-		UserFavoriteLocationsXref userFavoriteLocationsXref =locationService.createUserFavoriteLocation(userUUID,restaurantUUID);
+		UserFavoriteLocationsXref userFavoriteLocationsXref =locationService.createUserFavoriteLocation(userUUID,locationUUID);
 		return ResponseEntity.created(new URI("/v1/favorite/USERUUID/create/LOCATIONUUID4")).body(userFavoriteLocationsXref);
 	}
 	
-	@DeleteMapping(value="/v1/favorite/{uuid}/delete/{id}")
-	public  ResponseEntity<Object> deleteUserFavoriteLocation(@PathVariable(value="uuid") String userUUID, @PathVariable(value="id") String restaurantUUID) throws URISyntaxException{
+	@DeleteMapping(value="/v1/favorite/{userUUID}/delete/{locationUUID}")
+	public  ResponseEntity<Object> deleteUserFavoriteLocation(@PathVariable(value="userUUID") String userUUID, @PathVariable(value="locationUUID") String locationUUID) throws URISyntaxException{
 		
-		System.out.println("userUUID:"+userUUID+",restaurantUUID:"+restaurantUUID);
+		System.out.println("userUUID:"+userUUID+",locationUUID:"+locationUUID);
 		
-		long numOfRowsDeleted =locationService.deleteUserFavoriteLocation(userUUID,restaurantUUID);
+		long numOfRowsDeleted =locationService.deleteUserFavoriteLocation(userUUID,locationUUID);
 		System.out.println(" deleted rows #"+numOfRowsDeleted);
 		return ResponseEntity.noContent().build();
 	}
 
 	
-	@PostMapping(value="v1/favorite/list/{uuid}")
-	public  ResponseEntity<List<LocationSearchResponse>> getUserFavoriteLocation(@PathVariable(value="uuid") String userUUID, @RequestBody UserLocationInfo userLocationInfo, @RequestParam Integer start,@RequestParam  Integer limit){
+	@PostMapping(value="v1/favorite/list/{userUUID}")
+	public  ResponseEntity<List<LocationSearchResponse>> getUserFavoriteLocation(@PathVariable(value="userUUID") String userUUID, @RequestBody LocationSearchRequest locationSearchRequest, @RequestParam Integer start, @RequestParam  Integer limit){
 		
 		System.out.println("Getting list of favorite restaurants by userUUID:"+userUUID);
 		Pageable pageable = PageRequest.of(start, limit);
-		List<LocationSearchResponse> userFavoriteLocations=locationSearchService.getUserFavoriteLocation(userUUID,userLocationInfo, pageable);
+		List<LocationSearchResponse> userFavoriteLocations=locationSearchService.getUserFavoriteLocation(userUUID,locationSearchRequest, pageable);
 		System.out.println(" User Favorite locations list size #"+userFavoriteLocations.size());
 		return ResponseEntity.ok().body(userFavoriteLocations);
 	}
