d0568312c5ef3308298205f034a9a5b546804ba6
a6002148 <srilakshmi.kommineni@bestbuy.com>
2019-09-26 03:52:38 -0500

    User Locations CRUD operations

--- src/main/java/com/ivo/app/services/serviceimpl/UserPlacesServiceImpl.java
+++ src/main/java/com/ivo/app/services/serviceimpl/UserPlacesServiceImpl.java
@@ -2,6 +2,9 @@ package com.ivo.app.services.serviceimpl;
 
 import com.ivo.app.services.dao.UserPlacesDao;
 import com.ivo.app.services.domain.UserLocation;
+import com.ivo.app.services.entity.UserLocationsXref;
+import com.ivo.app.services.repository.UserLocationsXrefReepository;
+import com.ivo.app.services.request.UserLocationRequest;
 import com.ivo.app.services.service.UserPlacesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +17,29 @@ public class UserPlacesServiceImpl implements UserPlacesService {
     @Autowired
     private UserPlacesDao userPlacesDao;
 
+
+    @Autowired
+    private UserLocationsXrefReepository userLocationsXrefReepository;
+
     @Override
     public List<UserLocation> getUserPlaces(String userUUID) {
         return userPlacesDao.getUserPlaces(userUUID);
+    }
+
+    @Override
+    public UserLocationsXref createLableForMyPlaces(UserLocationRequest userLocation,String userUUID,
+                                                    Long useerAddrId) {
+
+        return userPlacesDao.createLableForMyPlaces(userLocation,userUUID,useerAddrId);
+    }
+   @Override
+   public int updateableForMyPlaces(UserLocationRequest userLocation,Long userAdrId,String userUUID) {
+
+       return userPlacesDao.updateLableForMyPlaces(userLocation,userAdrId);
+   }
+
+    @Override
+    public long deleteLableForMyPlaces(String userUUID, Long useerAdrId) {
+        return userLocationsXrefReepository.deleteByuserUuidAndUserAddrId(userUUID,  useerAdrId);
     }
 }
