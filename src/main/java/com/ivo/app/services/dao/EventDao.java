59626aab1a38e43a05e7a35000f813df0b9e9f82
sudha <sudha.nalamotu@gmail.com>
2019-10-18 02:45:05 -0500

    Added lookup and static validations to event controller

--- src/main/java/com/ivo/app/services/dao/EventDao.java
+++ src/main/java/com/ivo/app/services/dao/EventDao.java
@@ -2,7 +2,13 @@ package com.ivo.app.services.dao;
 
 import com.ivo.app.services.domain.EventDetailRequest;
 
+import java.time.LocalDateTime;
+
 public interface EventDao {
 
     int updateEvent(EventDetailRequest event, String userUUID);
+
+    boolean isEventConflictingAnyOtherExistingEvent(LocalDateTime eventStartTime, LocalDateTime localDateTime, String userUUID);
+
+    boolean isEventWithinUserDailyEventsLimit(String userUUID, LocalDateTime eventStartTime);
 }
--- src/main/java/com/ivo/app/services/repository/EventDetailsTransRepository.java
+++ src/main/java/com/ivo/app/services/repository/EventDetailsTransRepository.java
@@ -2,6 +2,9 @@ package com.ivo.app.services.repository;
 
 import com.ivo.app.services.entity.EventDetailsEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventDetailsTransRepository extends CrudRepository<EventDetailsEntity, Long> {
+
 }
